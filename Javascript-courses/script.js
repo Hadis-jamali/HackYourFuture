1970e2aee91a0dd45a1f79025e1bfcac7dfb7431
Hadis Jamali <hadisjamali95@gmail.com>
2023-11-03 14:11:53 +0100

    add javascript folder and practice ForEach and map

---
+++ Javascript-courses/script.js
@@ -0,0 +1,18 @@
+//forEach
+const numbers = [1, 2, 3, 4];
+const newNumbers = [];
+
+numbers.forEach((number) => {
+  newNumbers.push(number * 2);
+});
+console.log(newNumbers);
+
+//map
+// const numbers = [1, 2, 3, 4];
+// const newNumbers = numbers.map((number) => number * 2);
+// console.log(newNumbers);
+
+//find
+// const numbers = [1, 2, 3, 4];
+// const result = numbers.find((number)=>{number===5}) ;
+// console.log(result) ;
