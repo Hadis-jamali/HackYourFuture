9dfb7c44b80882421c0e1128d58b4b5d926a6676
Hadis Jamali <hadisjamali95@gmail.com>
2023-11-04 11:20:42 +0100

    reduce

--- Javascript-courses/script.js
+++ Javascript-courses/script.js
@@ -1,18 +1,23 @@
 //forEach
-const numbers = [1, 2, 3, 4];
-const newNumbers = [];
+// const numbers = [1, 2, 3, 4];
+// const newNumbers = [];
 
-numbers.forEach((number) => {
-  newNumbers.push(number * 2);
-});
-console.log(newNumbers);
+// numbers.forEach((number) => {
+//   newNumbers.push(number * 2);
+// });
+// console.log(newNumbers);
 
 //map
 // const numbers = [1, 2, 3, 4];
 // const newNumbers = numbers.map((number) => number * 2);
 // console.log(newNumbers);
 
-//find
+//find & filter (boolean migirn)
 // const numbers = [1, 2, 3, 4];
 // const result = numbers.find((number)=>{number===5}) ;
 // console.log(result) ;
+
+// reduce
+const numbers = [1, 2, 3, 4];
+const result= number.reduce((acc,cur)=>{acc+cur} , 0);
+console.log(result);
