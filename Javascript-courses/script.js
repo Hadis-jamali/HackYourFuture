2a123afd252a9472dd11e7320cb9f674be8f4b3a
Hadis Jamali <hadisjamali95@gmail.com>
2023-11-06 18:51:23 +0100

    add a future age calculator

--- Javascript-courses/script.js
+++ Javascript-courses/script.js
@@ -29,10 +29,41 @@
 
 let userName = "hadis";
 let id = "10";
-let age = 30;
+let fav = "music";
 let taxtRate = 0.34;
 let isName = true;
 console.log("Hadis Jamali " + userName);
-console.log(age + taxtRate + " units");
+console.log(fav + taxtRate + " units");
 console.log(+id + age);
-console.log(typeof id) ;
+console.log(typeof id);
+
+let user1 = ["hadis", "jamali", "reza"];
+user1[user1.length - 1]; //akharin khone
+
+//Age-ify (A future age calculator)
+
+const yearOfBirth = 1995;
+const yearFuture = 2045;
+console.log(typeof yearFuture);
+const curentYear = 2023;
+const age = curentYear - yearOfBirth;
+console.log(age);
+const futureAge = yearFuture - curentYear + age;
+console.log(futureAge);
+console.log(`You will be ${futureAge} years old in ${yearFuture}`);
+
+//Goodboy-Oldboy (A dog age calculator)
+
+const dogYearOfBirth = 2018;
+const dogYearFuture = 2045;
+const dogYear = 2023;
+const result = dogYear - dogYearOfBirth;
+console.log(result);
+const dogFutureAge = dogYearFuture - dogYear + result;
+console.log(dogFutureAge);
+const shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears === true) {
+  console.log(`Your dog will be ${dogFutureAge} dog years old in ${dogYearFuture}`);
+} else {
+  console.log(`Your dog will be ${dogFutureAge} human years old in ${dogYearFuture}`);
+}
