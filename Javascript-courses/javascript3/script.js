fd8564810205ab5f8148ee0717b5165507f3286a
Hadis Jamali <hadisjamali95@gmail.com>
2024-01-07 14:25:46 +0100

    add fetch

--- Javascript-courses/javascript3/script.js
+++ Javascript-courses/javascript3/script.js
@@ -40,40 +40,69 @@
 //fetchData(processData);
 
 //practice
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
+// const button1 = document.getElementById("button1");
+// const button2 = document.getElementById("button2");
+// const button3 = document.getElementById("button3");
 
-let clickCount = 0;
-function handlerClick() {
-  clickCount++;
-  console.log(clickCount);
-}
+// let clickCount = 0;
+// function handlerClick() {
+//   clickCount++;
+//   console.log(clickCount);
+// }
 
-button1.addEventListener("click", handlerClick);
-button2.addEventListener("click", handlerClick);
-button3.addEventListener("click", handlerClick3);
+// button1.addEventListener("click", handlerClick);
+// button2.addEventListener("click", handlerClick);
+// button3.addEventListener("click", handlerClick3);
 
-function handlerClick3() {
-  setTimeout(() => {
-    console.log("This text was delayed by 3 seconds");
-  }, 3000);
-}
+// function handlerClick3() {
+//   setTimeout(() => {
+//     console.log("This text was delayed by 3 seconds");
+//   }, 3000);
+// }
 
-//w3 school
-function validateForm() {
-  let x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
+// //w3 school
+// function validateForm() {
+//   let x = document.forms["myForm"]["fname"].value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//   }
+// }
 
+// function sum(...args) {
+//   let sum = 0;
+//   for (let arg of args) sum += arg;
+//   return sum;
+// }
 
-function sum(...args) {
-  let sum = 0;
-  for (let arg of args) sum += arg;
-  return sum;
-}
+// let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
 
-let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
+//  [
+//    {
+//     "orderName" : "hadis" ,
+//     "orderId" : 1 ,
+//      "orderPrice" : "40kr" ,
+//     "listDrink" : {
+//       "name" :"coca cola"
+//     } ,
+//     {
+//       "name" :"cofee"
+//     }
+//      "extras" :{
+//        "chees" : 5
+//      }
+//    }
+
+//  ]
+const astronauts = document.getElementById("astronaut");
+
+fetch("http://api.open-notify.org/astros.json")
+  .then((Response) => Response.json())
+  .then((data) => {
+    const people = data.people;
+    people.forEach((astronaut) => {
+      const pTag = document.createElement("p");
+      pTag.innerText = astronaut.name;
+      astronauts.appendChild(pTag);
+    });
+  });
