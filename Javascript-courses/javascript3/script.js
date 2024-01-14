694c72e9810fdfc64257e1b9aa40fc6be678a3f5
Hadis Jamali <hadisjamali95@gmail.com>
2024-01-14 16:01:02 +0100

    add some function in javascript 3 in class

--- Javascript-courses/javascript3/script.js
+++ Javascript-courses/javascript3/script.js
@@ -94,15 +94,146 @@
 //    }
 
 //  ]
-const astronauts = document.getElementById("astronaut");
 
-fetch("http://api.open-notify.org/astros.json")
-  .then((Response) => Response.json())
-  .then((data) => {
-    const people = data.people;
-    people.forEach((astronaut) => {
-      const pTag = document.createElement("p");
-      pTag.innerText = astronaut.name;
-      astronauts.appendChild(pTag);
-    });
-  });
+// const astronauts = document.getElementById("astronaut");
+
+// fetch("http://api.open-notify.org/astros.json")
+//   .then((Response) => Response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const people = data.people;
+//     people.forEach((astronaut) => {
+//       const pTag = document.createElement("p");
+//       pTag.innerText = astronaut.name;
+//       astronauts.appendChild(pTag);
+//     });
+//   });
+
+// setInterval(() => {
+//   fetch("https://dog.ceo/api/breeds/image/random")
+//     .then((Response) => Response.json())
+//     .then((data) => {
+//       console.log(data);
+//       const img = document.createElement("img");
+//       img.src = data.message;
+//       dogImage.appendChild(img);
+
+//     });
+//     dogImage.innerText = "";
+// }, 1000);
+
+// const getImage = () => {
+//   fetch("https://dog.ceo/api/breeds/image/random")
+//     .then((response) => response.json())
+//     .then((json) => {
+//       // const img = document.createElement("img");
+//       const img = document.getElementById("dog-image");
+//       img.src = json.message;
+//     });
+// };
+
+// getImage();
+
+// setInterval(() => {
+//   getImage();
+// }, 2000);
+
+// function getData() {
+//   fetch("http://api.open-notify.org/astros.json")
+//     .then((response) => response.json())
+//     .then((json) => {
+//       updateList(json);
+//     });
+// }
+
+//3A Unit test pattern
+// test("AdditionTest", () => {
+//   expect(add(1, 2)).toBe(3);
+// });
+
+// test("AdditionTest", () => {
+//   //Arrange
+//   const a = 1;
+//   const b = 2;
+//   //Act
+//   const c = add(a, b);
+// });
+
+// const promise = new promise(function (resolve, reject) {
+//   resolve("my result");
+// });
+
+// function wait(milliseconds) {
+//   return new Promise((resolve) => setTimeout(resolve, milliseconds));
+// }
+
+// const printAfter4Second = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve();
+//   }, 4000);
+// });
+
+// printAfter4Second.then(() => {
+//   console.log("hello");
+// });
+
+// const print = new Promise((resolve, reject) => {
+//   if (2 + 3 === 4) {
+//     resolve("its 4");
+//   } else {
+//     reject("its not 4");
+//   }
+// });
+// print
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const answer = document.getElementById("answer");
+
+// fetch("https://yesno.wtf/api")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     getData(json.answer);
+//   });
+
+// function getData(answer) {
+//   console.log(answer);
+//   if (answer === "yes") {
+//     fetch("https://knajskdskj.jasdk")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         alert(json);
+//       })
+//       .catch((err) => {
+//         answer.innerText = err;
+//       });
+//     return;
+//   }
+
+//   answer.innerText = answer;
+// }
+
+// async function getYesNo() {
+//   try {
+//     const response = await fetch("https://dog.ceo/api/breeds/image/random");
+//     const json = await response.json();
+//     console.log("response", json);
+//   } catch (error) {
+//     console, error(error);
+//   }
+// }
+// getImage();
+
+async function astronauts() {
+  const response = await fetch(
+    "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
+  );
+  const json = await response.json();
+  console.log(json);
+}
+
+astronauts();
