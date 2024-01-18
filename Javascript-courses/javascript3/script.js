501fb328bb835c154aefedbe54fc9b45231f0150
Hadis Jamali <hadisjamali95@gmail.com>
2024-01-19 00:47:44 +0100

    add setInterval

--- Javascript-courses/javascript3/script.js
+++ Javascript-courses/javascript3/script.js
@@ -228,12 +228,16 @@
 // }
 // getImage();
 
-async function astronauts() {
-  const response = await fetch(
-    "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
-  );
-  const json = await response.json();
-  console.log(json);
-}
+// async function astronauts() {
+//   const response = await fetch(
+//     "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
+//   );
+//   const json = await response.json();
+//   console.log(json);
+// }
 
-astronauts();
+// astronauts();
+
+const interval = setInterval(() => {
+  console.log("hello");
+}, 600);
