99362cce71e790f518d940bda0bf5963fe13f67d
Hadis Jamali <hadisjamali95@gmail.com>
2024-02-11 15:43:31 +0100

    add new database (MongoDB)

--- database-p/homework.sql
+++ database-p/homework.sql
@@ -159,3 +159,33 @@ CREATE TABLE staff_title(
     FOREIGN KEY (staff_id) REFERENCES staff(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (title_id) REFERENCES title(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
+
+--------------------------- MongoDb
+
+-- db.users.insertOne({ name: "Lawan", email: "lawansubba@gmail.com"});
+-- db.users.insertOne({ name: "harry", email: "harry@gmail.com", age: 20 });
+-- db.users.insertOne({ name: "bob", email: "bob@gmail.com", age: 20, address: "Aarhus" });
+-- db.users.insertOne({ name: "hadis", address: "riskov aarhus", age: 23, country: "Denmark" });
+
+
+-- // -- MongoDb
+-- db.users.find({name: 'bob'});
+-- // -- MySQL Select * from users;
+
+
+
+-- db.users.countDocuments({age:20});
+
+-- db.users.updateOne({name :'bob'}, {$set :{age:30}}) ;
+
+-- db.users.deleteOne({age:20});
+
+-- db.users.updateOne({name :'hadis'}, {$set :{address:'alborg'}}) ;
+
+-- db.users.find({name: 'hadis'});
+
+-- db.users.updateOne({name :'hadis'}, {$set :{address:'alborg'}}) ;
+
+-- db.users.deleteOne({address:'alborg'});
+
+-- db.users.find({"age" : {"$gte" : 10, "$lte" : 30}});
