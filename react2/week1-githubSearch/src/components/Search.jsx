ff18d3516e6a27fc215e2e9edca04a86e4e69664
Hadis Jamali <hadisjamali95@gmail.com>
2024-04-24 22:53:01 +0200

    add searchHandler

--- react2/week1-githubSearch/src/components/Search.jsx
+++ react2/week1-githubSearch/src/components/Search.jsx
@@ -3,7 +3,19 @@ import { FaSearch } from "react-icons/fa";
 import "./Search.css";
 function Search() {
   const [search, setSearch] = useState([]);
-  
+  const searchHandler = () => {
+    if (search) {
+      let searchMeals = {
+        data: meals.data,
+      };
+      searchMeals.data = meals.data.filter((meal) => {
+        return meal.title.toLowerCase().includes(search);
+      });
+      setMeals(searchMeals);
+    } else {
+      setMeals(meals);
+    }
+  };
   return (
     <div className="search">
       <input
